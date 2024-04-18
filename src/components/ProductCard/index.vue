89ffd596ccc6f2b410b58eeb4cc16db701c7e1af
dtthanh257 <80798006+dtthanh257@users.noreply.github.com>
2024-04-19 06:04:09 +0700

    'update`

---
+++ src/components/ProductCard/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="product-card flex-column">
+    <div class="product-card-code">Mã đơn hàng</div>
+
+    <div class="flex-column">
+      <h3>[Tên đăng nhập người bán]</h3>
+
+      <div
+        class="flex-row gap-20"
+        style="padding-left: 40px; padding-bottom: 45px"
+      >
+        <div class="product-card-img"></div>
+        <div class="product-card-info flex-column">
+          <div class="product-card-name">[Tên sản phẩm]</div>
+          <div class="product-card-quantity">x[Số lượng sản phẩm]</div>
+          <div class="product-card-status">Trạng thái:</div>
+          <div class="product-card-price">[Giá sản phẩm]</div>
+        </div>
+      </div>
+      <div class="product-cart-payload gap-8">
+        <p>Thành tiền:</p>
+        <div class="product-cart-pay">[Giá sản phẩm +phí ship]</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ProductCard",
+};
+</script>
--- src/pages/ProductManage/index.vue
+++ src/pages/ProductManage/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="background-color: #f2f2f3">
     <Navbar> </Navbar>
     <div class="grid-12" style="background-color: #f2f2f3">
       <div class="product-manage-contain home-cate">
@@ -20,33 +20,112 @@
           </div>
         </div>
       </div>
+      <div
+        id="donmuaTrue"
+        class="flex-column home-cate gap-20"
+        style="margin-bottom: 40px"
+      >
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+      </div>
+      <div id="donbanTrue" class="flex-column home-cate gap-20">
+        <div class="flex-row reli-acti">
+          <button
+            class="reli-acti-dangban"
+            :class="{ isTrue: dangban }"
+            @click="toggleDanBan"
+          >
+            Đăng bán
+          </button>
+          <button
+            class="reli-acti-thumua"
+            :class="{ isTrue: thumua }"
+            @click="toggleThuMua"
+          >
+            Thu mua
+          </button>
+          <button
+            class="reli-acti-thugom"
+            :class="{ isTrue: thugom }"
+            @click="toggleThuGom"
+          >
+            Thu gom
+          </button>
+        </div>
+      </div>
     </div>
-    <Footer></Footer>
+    <div class="grid-12"></div>
+    <Footer
+      style="background-color: #f2f2f3; margin-bottom: 0; padding-bottom: 128px"
+    ></Footer>
   </div>
 </template>
 <script>
 import Navbar from "@/components/Navbar/index.vue";
 import Footer from "@/components/Footer/index.vue";
+import ProductCard from "@/components/ProductCard/index.vue";
 export default {
   name: "ProductManage",
   data() {
     return {
-      actiBuy: false,
+      actiBuy: true,
       actiSell: false,
+      dangban: true,
+      thumua: false,
+      thugom: false,
+      donmua: null,
+      donban: null,
     };
   },
   components: {
     Navbar,
     Footer,
+    ProductCard,
+  },
+  mounted() {
+    this.donmua = document.querySelector("#donmuaTrue");
+    this.donban = document.querySelector("#donbanTrue");
   },
   methods: {
     toggleActiBuy() {
       this.actiBuy = !this.actiBuy;
-      this.actiSell = false; // Đảm bảo chỉ có một trong hai trạng thái được kích hoạt
+      this.actiSell = false;
+      if (this.actiBuy == true) {
+        console.log(this.donmua);
+        this.donmua.style.display = "flex";
+        this.donban.style.display = "none";
+      } else {
+        this.donmua.style.display = "none";
+        this.donban.style.display = "flex";
+      }
     },
     toggleActiSell() {
       this.actiSell = !this.actiSell;
-      this.actiBuy = false; // Đảm bảo chỉ có một trong hai trạng thái được kích hoạt
+      this.actiBuy = false;
+      console.log(this.dangban);
+      if (this.actiBuy == true) {
+        this.donmua.style.display = "flex";
+        this.donban.style.display = "none";
+      } else {
+        this.donmua.style.display = "none";
+        this.donban.style.display = "flex";
+      }
+    },
+    toggleDanBan() {
+      this.dangban = true;
+      this.thumua = false;
+      this.thugom = false;
+    },
+    toggleThuMua() {
+      this.thumua = true;
+      this.dangban = false;
+      this.thugom = false;
+    },
+    toggleThuGom() {
+      this.thugom = true;
+      this.dangban = false;
+      this.thumua = false;
     },
   },
 };
